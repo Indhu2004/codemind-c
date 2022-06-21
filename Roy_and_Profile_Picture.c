03491f3ff84c64c402063020a5dfe3ac04ce6327
Indhu2004 <96334997+Indhu2004@users.noreply.github.com>
2022-06-21 23:05:00 +0530

    #include<stdio.h> int main() {     int i,I,w,h,n;     scanf("%d",&I);     scanf("%d",&n);     for(i=1;i<=n;i++)     {         scanf("%d%d",&h,&w);         if((w<I)||(h<I))         {             printf("UPLOAD ANOTHER ");         }         else         {          if(w==h)          {              printf("ACCEPTED ");          }          else          {              printf("CROP IT ");          }         }     } }

---
+++ Roy_and_Profile_Picture.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+    int i,I,w,h,n;
+    scanf("%d",&I);
+    scanf("%d",&n);
+    for(i=1;i<=n;i++)
+    {
+        scanf("%d%d",&h,&w);
+        if((w<I)||(h<I))
+        {
+            printf("UPLOAD ANOTHER
+");
+        }
+        else
+        {
+         if(w==h)
+         {
+             printf("ACCEPTED
+");
+         }
+         else
+         {
+             printf("CROP IT
+");
+         }
+        }
+    }
+}
