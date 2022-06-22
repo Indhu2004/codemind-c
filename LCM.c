345991fc057548c8e8f0d00812c71bc38bee014f
Indhu2004 <96334997+Indhu2004@users.noreply.github.com>
2022-06-22 08:06:02 +0530

    #include<stdio.h> int main() {     int num1,num2,lcm,gcd,temp;     scanf("%d%d",&num1,&num2);     int a=num1,b=num2;     while(num2!=0)     {         temp=num2;         num2=num1%num2;         num1=temp;     }     gcd=num1;     lcm=(a*b)/gcd;     printf("%d",lcm); }

---
+++ LCM.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int num1,num2,lcm,gcd,temp;
+    scanf("%d%d",&num1,&num2);
+    int a=num1,b=num2;
+    while(num2!=0)
+    {
+        temp=num2;
+        num2=num1%num2;
+        num1=temp;
+    }
+    gcd=num1;
+    lcm=(a*b)/gcd;
+    printf("%d",lcm);
+}
